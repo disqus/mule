456151fbad3c366a432ad6ea43a6c8c5cc7d1291
David Cramer <dcramer@gmail.com>
2011-05-04 18:26:50 -0700

    Proper warning formatting

--- mule/utils/conf.py
+++ mule/utils/conf.py
@@ -5,5 +5,5 @@ import warnings
 def configure(**kwargs):
     for k, v in kwargs.iteritems():
         if not hasattr(conf, k):
-            warnings.warn('Setting %k which is not defined by Mule', k)
+            warnings.warn('Setting %k which is not defined by Mule' % k)
         setattr(conf, k, v)
